696200dd84e0e6c0ac09ddfbc54fe2abfca1422d
mayanghui <10432633@nhu.edu.tw>
2018-11-06 11:55:27 +0800

    1414
    141

--- sketch2.js
+++ sketch2.js
@@ -11,6 +11,23 @@ KNN Image Classifier example with p5.js
 
 let knn;
 let video;
+const btn = document.getElementById("buttonNew")
+const cdn = document.getElementById("countdown")
+var countdown_num = 5;
+
+function countdown_f(){
+	cdn.innerHTML = countdown_num;
+	if (countdown_num==0){
+		cdn.style.display = "none";
+		predict();
+		clearTimeout(ct);
+		
+	}else{countdown_num--;
+	ct = setTimeout(countdown_f,1000);
+		
+	}
+	
+}
 
 function setup() {
   noCanvas();
@@ -32,8 +49,9 @@ function createButtons() {
 
 
 function restart() {
-
-   predict();
+	countdown_f();
+	btn.style.display = "none";
+    predict();
 
 }
 
@@ -69,9 +87,8 @@ function gotResults(results) {
   }
   //select('#result').html(msg);
 
-  setTimeout(function(){
-    predict();
-  }, 50);
+  
+  win_t = setTimeout(win,3000);
 }
 
 // Clear the data in one class
@@ -79,3 +96,11 @@ function clearClass(classIndex) {
   knn.clearClass(classIndex);
 }
 
+function win(){
+	cdn.innerHTML = "WIN!";
+	cdn.style.display = "";
+	countdown_num = 5;
+	document.getElementById("myImg").src = "";
+	btn.style.display = "";
+	
+}
